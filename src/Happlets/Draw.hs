7f4cafe160308bdfe20147225c49de45dcaec822
RaminHAL9001 <ramin.honary@gmail.com>
2020-05-05 02:13:01 +0900

    Added a LineStyle data type to the Happlets.Draw module.

--- src/Happlets/Draw.hs
+++ src/Happlets/Draw.hs
@@ -7,11 +7,16 @@
 -- library that can export to and import from SVG data, however no plans for such an extension have
 -- been made at the time of this writing.
 module Happlets.Draw
-  ( module Happlets.Draw.Color
-  , module Happlets.Draw.SampCoord
-  , module Happlets.Draw.Text
-  , module Happlets.Draw.Types2D
-  , Happlet2DGraphics(..), modifyPoint,
+  ( -- * 2D Graphics Typeclass
+    Happlet2DGraphics(..), modifyPoint,
+    -- * Line Stype
+    HasLineStyle(..), LineStyle(..), theLineColour,
+    makeLineStyle, lineColor, lineColour, lineWeight,
+    -- * Re-Exports
+    module Happlets.Draw.Color,
+    module Happlets.Draw.SampCoord,
+    module Happlets.Draw.Text,
+    module Happlets.Draw.Types2D
   ) where
 
 import           Happlets.Draw.Color
@@ -19,6 +24,7 @@ import           Happlets.Draw.SampCoord
 import           Happlets.Draw.Text
 import           Happlets.Draw.Types2D
 
+import           Control.Lens (Lens', lens)
 import           Control.Monad.IO.Class
 
 ----------------------------------------------------------------------------------------------------
@@ -39,14 +45,14 @@ class (Functor render, Applicative render, Monad render, MonadIO render)
   clearScreen :: FillColor -> render ()
 
   -- | Draw a line on the canvas.
-  drawLine :: RealFrac n => LineColor -> LineWidth n -> Line2D n -> render ()
+  drawLine :: RealFrac n => LineStyle n -> Line2D n -> render ()
 
   -- | Draw a sequence of lines from a list of points, from head-to-tail, on the canvas. Passing an
   -- empty list should result in a no-op. Passing a list with a single point should paint the point.
-  drawPath :: RealFrac n => LineColor -> LineWidth n -> [Point2D n] -> render ()
+  drawPath :: RealFrac n => LineStyle n -> [Point2D n] -> render ()
 
   -- | Draw a rectangle on the canvas.
-  drawRect :: RealFrac n => LineColor -> LineWidth n -> FillColor -> Rect2D n -> render ()
+  drawRect :: RealFrac n => LineStyle n -> FillColor -> Rect2D n -> render ()
 
   -- | Update a pixel value somewhere on the canvas.
   setPoint :: RealFrac n => Point2D n -> Color -> render ()
@@ -58,3 +64,37 @@ modifyPoint
   :: (Monad render, Happlet2DGraphics render, RealFrac n)
   => Point2D n -> (Color -> Color) -> render ()
 modifyPoint p f = getPoint p >>= setPoint p . f
+
+----------------------------------------------------------------------------------------------------
+
+-- | Provides a lens for changing the colour of various things.
+class HasLineStyle a where { lineStyle :: Lens' (a num) (LineStyle num); }
+
+data LineStyle num
+  = LineStyle
+    { theLineColor  :: !Color
+    , theLineWeight :: !num
+      -- ^ The weight specified in pixels
+    }
+  deriving (Eq, Show, Read)
+
+instance HasLineStyle LineStyle where { lineStyle = lens id $ flip const; }
+
+theLineColour :: LineStyle num -> Color
+theLineColour = theLineColor
+
+makeLineStyle :: Num num => LineStyle num
+makeLineStyle = LineStyle
+  { theLineColor  = packRGBA32 0xA0 0xA0 0xA0 0xA0
+  , theLineWeight = 2
+  }
+
+lineColor :: HasLineStyle line => Lens' (line num) Color
+lineColor = lineStyle . lens theLineColor (\ a b -> a{ theLineColor = b })
+
+lineColour :: HasLineStyle line => Lens' (line num) Color
+lineColour = lineColor
+
+lineWeight :: HasLineStyle line => Lens' (line num) num
+lineWeight = lineStyle . lens theLineWeight (\ a b -> a{ theLineWeight = b })
+
